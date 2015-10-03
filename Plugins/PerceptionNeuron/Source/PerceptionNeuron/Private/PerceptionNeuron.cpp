b7f20616187abc2ba98dd4e3ce903cb68d5568b3
Cyx69 <hfink@web.de>
2015-10-04 00:16:31 +0200

    Add Logmessage on Module Start and Stop

--- Plugins/PerceptionNeuron/Source/PerceptionNeuron/Private/PerceptionNeuron.cpp
+++ Plugins/PerceptionNeuron/Source/PerceptionNeuron/Private/PerceptionNeuron.cpp
@@ -8,12 +8,10 @@
 // Perception Neuron (TM) is a trademark of Beijing Noitom Technology Ltd.
 // 
 // Description:
-// This code implements the module startup and shutdown functions.
+// This code implements the Perception Neuron (TM) module startup and shutdown functions.
 // 
 
 #include "PerceptionNeuronPrivatePCH.h"
-
-
 
 #define LOCTEXT_NAMESPACE "FPerceptionNeuronModule"
 
@@ -21,7 +19,7 @@ void FPerceptionNeuronModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
 	
-	
+	UE_LOG(LogInit, Log, TEXT("Unofficial Perception Neuron (TM) Plugin started."));
 }
 
 void FPerceptionNeuronModule::ShutdownModule()
@@ -29,7 +27,7 @@ void FPerceptionNeuronModule::ShutdownModule()
 	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
 	// we call this function before unloading the module.
 	
-	
+	UE_LOG(LogExit, Log, TEXT("Unofficial Perception Neuron (TM) Plugin unloaded."));
 }
 
 #undef LOCTEXT_NAMESPACE
