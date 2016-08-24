bb86461f7b2b12428ecfd1316028b191441abbdd
Cyx69 <hfink@web.de>
2016-08-24 13:02:28 +0200

    Support for older versions

--- Source/ThirdPersonNeuron/ThirdPersonNeuronCharacter.cpp
+++ Source/ThirdPersonNeuron/ThirdPersonNeuronCharacter.cpp
@@ -3,6 +3,7 @@
 #include "ThirdPersonNeuron.h"
 #include "Kismet/HeadMountedDisplayFunctionLibrary.h"
 #include "ThirdPersonNeuronCharacter.h"
+#include "Runtime/Launch/Resources/Version.h"
 
 //////////////////////////////////////////////////////////////////////////
 // AThirdPersonNeuronCharacter
@@ -29,13 +30,21 @@ AThirdPersonNeuronCharacter::AThirdPersonNeuronCharacter()
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+#if (ENGINE_MAJOR_VERSION >= 4) && (ENGINE_MINOR_VERSION >= 12)
 	CameraBoom->SetupAttachment(RootComponent);
+#else
+	CameraBoom->AttachTo(RootComponent);
+#endif
 	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
 	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
 
 	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+#if (ENGINE_MAJOR_VERSION >= 4) && (ENGINE_MINOR_VERSION >= 12)
 	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+#else
+	FollowCamera->AttachTo(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+#endif
 	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
